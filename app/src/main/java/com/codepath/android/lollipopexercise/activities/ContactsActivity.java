88c6205ffebfe826e189e6d5b676b342b949020f
Kenneth Nturibi <knturibi@fb.com>
2018-07-12 11:55:57 -0700

    Add all files

---
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/ContactsActivity.java
@@ -0,0 +1,97 @@
+package com.codepath.android.lollipopexercise.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.adapters.ContactsAdapter;
+import com.codepath.android.lollipopexercise.models.Contact;
+
+import java.util.List;
+
+
+public class ContactsActivity extends AppCompatActivity {
+    private RecyclerView rvContacts;
+    private ContactsAdapter mAdapter;
+    private List<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        // Find RecyclerView and bind to adapter
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // allows for optimizations
+        rvContacts.setHasFixedSize(true);
+
+        // Define 2 column grid layout
+        final GridLayoutManager layout = new GridLayoutManager(ContactsActivity.this, 2);
+
+        // Unlike ListView, you have to explicitly give a LayoutManager to the RecyclerView to position items on the screen.
+        // There are three LayoutManager provided at the moment: GridLayoutManager, StaggeredGridLayoutManager and LinearLayoutManager.
+        rvContacts.setLayoutManager(layout);
+
+        // get data
+        contacts = Contact.getContacts();
+
+        // Create an adapter
+        mAdapter = new ContactsAdapter(ContactsActivity.this, contacts);
+
+        // Bind adapter to list
+        rvContacts.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miCompose:
+                onComposeAction(item);
+                return true;
+            case R.id.home:
+                finishAfterTransition();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onComposeAction(MenuItem mi) {
+        // handle click here
+        Contact contact = Contact.getRandomContact(getApplicationContext());
+        contacts.add(0, contact);
+        mAdapter.notifyItemInserted(0);
+
+
+        View.OnClickListener myOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ContactActivity", "About to undo");
+                contacts.remove(0);
+                mAdapter.notifyItemRemoved(0);
+            }
+        };
+        Snackbar.make(rvContacts, "Added: "+contact.getName(), Snackbar.LENGTH_LONG)
+                .setAction("UNDO", myOnClickListener)
+                .show();
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -0,0 +1,91 @@
+package com.codepath.android.lollipopexercise.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.models.Contact;
+
+public class DetailsActivity extends AppCompatActivity {
+    public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
+    private Contact mContact;
+    private ImageView ivProfile;
+    private TextView tvName;
+    private TextView tvPhone;
+    private View vPalette;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        ivProfile = (ImageView) findViewById(R.id.ivProfile);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvPhone = (TextView) findViewById(R.id.tvPhone);
+        vPalette = findViewById(R.id.vPalette);
+
+        // Extract contact from bundle
+        mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
+
+        // Fill views with data
+//        Glide.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).centerCrop().into(ivProfile);
+        tvName.setText(mContact.getName());
+        tvPhone.setText(mContact.getNumber());
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+
+
+
+        // Define a listener for image loading
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                // TODO 1. Insert the bitmap into the profile image view
+                ivProfile.setImageBitmap(resource);
+
+                // TODO 2. Use generate() method from the Palette API to get the vibrant color from the bitmap
+                // Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                Palette palette = Palette.from(resource).generate();
+                // Pick one of the swatches
+                Palette.Swatch vibrant = palette.getVibrantSwatch();
+                if (vibrant != null) {
+                    // Set the background color of a layout based on the vibrant color
+                    vPalette.setBackgroundColor(vibrant.getRgb());
+                }
+            }
+        };
+        // Store the target into the tag for the profile to ensure target isn't garbage collected prematurely
+        ivProfile.setTag(target);
+        // Instruct Picasso to load the bitmap into the target defined above
+        Glide.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).asBitmap().centerCrop().into(target);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
